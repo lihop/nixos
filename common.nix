1c57e295a950a7f63f9231058013e495d2441225
Leroy Hopson <leroy@leroy.geek.nz>
2015-02-03 13:20:52 +1200

    Enable pulse audio.

--- common.nix
+++ common.nix
@@ -53,6 +53,8 @@
 
   virtualisation.docker.enable = true;
 
+  hardware.pulseaudio.enable = true;
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.extraUsers.leroy = {
     isNormalUser = true;
