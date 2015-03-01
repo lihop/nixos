aea8e448ad5146e8963c40958538aa340a50d205
Leroy Hopson <git@leroy.geek.nz>
2015-03-01 23:47:07 +1200

    Use all available cores for builds

--- roles/common.nix
+++ roles/common.nix
@@ -25,10 +25,13 @@
     uid = 1000;
     openssh.authorizedKeys.keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCsJ44pUGqS8r73K0UuhTl9S7o2hENdSATug45Vb28UhuuBaiVIF8w0o7Q/sa0DhBowKB26Rre+9GJrvgglh4B3NcF/rlS9sHUAfyaAJ6bmp281buXL3FQEz4jYjfoxgX/n4aTTCfGf27eegQo/BF45aTFZEetcCQLTkh2HeihNOAMf+eUS+hDTdCvMHv2+spo9BTnogbCAy4V3Joka9tc3oskayneeY7vGhPbAMqCrI9mLRWN2f9vu8KEONEoJbhBmk8yoVW0rbICxfzNICWTJUEh4UV4R0We5eS2qhdXjSzVQf/pzQzNlOWkVL+tvi7P3KDFFrBli55qAwpE+y4qW0orao4E4f5bPcHHr5GbDQI5YN+V6DNBjT83S3t3LRafSFLGsLz1WYiAqQynDZbiigC2Kw7jzKWQ20qmwWTBQDfyhQM0JKeoyquEbCjlc7bV8vASJDPPzOe/uAVRC96FuqlGv665y+cpNEyWomFG1AyjzZAhh8e7//4F5bXbb/keQS3wnYXUq6wy0L9KYdhJzlFjySl3muZJnH8IL3vrSrI2tri/SQQwmATIM3NoMd4l9co2opchQkW2XMaVfU7yDdvt5Mkzp9/HqASjmVPW0G+aPokjyb1J+DfboeKAtwcJ/es2aRDNKIPWx2vH5r4/WzRzEgrdtHrwtz1eEN1wKDw== leroy@hilly"
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC6cV2Zqqzk/w1F5pvZ8Gvdi64/izXQkZI7+rtDV0EyjG4p6d3V4ecTyWjZq02m6CWY/gUW20joktzMDKoRfTT4xpuwRl2rYQ78jrnbZE+GhHyJLlPA5NiWouL6cIqOHyhme1OctlQQuS4lzdnwHamHaFOblDw7pJgfOgcwKPYYUYd6HH9ILtZoD2Xd9SPZWs3ildZanBO2VIdMxJAB/5yEORvVkEqYJWsNQOefDoMbZU/vwiO9dnOU6i33m1ZDOb/fes2zi9CsGn7DAdEJzN/IgJvdln11XV8p2nf+y73Nw2sdsgP6qWQyDJnZpkvJ8kYd2P14QVD72uR5F5V+7CCoOjw/BaMCknnLgRObRUDTzYMd1CPGqtsLtyc+vU+ErSU2m8kLtU1JAcvDwtj+9tociptSI4wAVBVwbX7kdlj5/6d/QDqStGWh0eeSCyUxwCnpENrrgAImzazbLJ1XDYaZjv/zJdkUm8d/6lP39ijC6z5S7C9YzDIsgim8mGtHygLtXe2HQfKxvphlIDLv8DiJfncWZ/SG7xqj9yDQojjFBCW4ZzJLPEWXcpEUB3xpa9+bdu1ewySgYSPZJadeJkysrchLwosSmzDFCbhQCYhm6+TREorYiW2oQmUNRCGf0iRDMrysRlksyKZBi3CPsCSm6dEINXbn/LZZ5toGPYAVww== root@hilly"
     ];
   };
   
   nix.extraOptions = ''
     auto-optimise-store = true
   '';
+
+  nix.buildCores = 0;
 }
