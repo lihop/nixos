eea9f874990e5d96a34303b11a574fb82a236f8c
Leroy Hopson <git@leroy.geek.nz>
2015-03-15 11:48:12 +1200

    Added virtualbox

--- roles/virtualisation.nix
+++ roles/virtualisation.nix
@@ -2,4 +2,9 @@
 
 {
   virtualisation.docker.enable = true;
+  services.virtualboxHost.enable = true;
+
+  # In order to pass USB devices from the host to the guests,
+  # the user needs to be in the vboxusers group
+  users.extraGroups.vboxusers.members = [ "leroy" ];
 }
