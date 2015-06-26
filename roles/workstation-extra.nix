b1e65877ff91f3d294dd6fad177ccffadf2788c4
Leroy Hopson <git@leroy.geek.nz>
2015-06-26 12:01:26 +1200

    Merge virtualisation role into workstation-extra

--- roles/workstation-extra.nix
+++ roles/workstation-extra.nix
@@ -7,4 +7,12 @@
     taskwarrior
     transmission
   ];
+
+  virtualisation.docker.enable = true;
+  services.virtualboxHost.enable = true;
+  virtualisation.libvirtd.enable = true;
+
+  # In order to pass USB devices from the host to the guests,
+  # the user needs to be in the vboxusers group
+  users.extraGroups.vboxusers.members = [ "leroy" ];
 }
