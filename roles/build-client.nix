04f689f1b1fab1c203ce1924963656feba6c0861
Leroy Hopson <git@leroy.geek.nz>
2015-09-02 23:20:34 +1200

    Use dynamic dns domain name of build server

--- roles/build-client.nix
+++ roles/build-client.nix
@@ -4,7 +4,7 @@
   nix.distributedBuilds = true;
 
   nix.buildMachines = [
-    { hostName = "zeno"; maxJobs = 8; sshKey = "/root/.ssh/id_rsa"; sshUser = "leroy"; system = "x86_64-linux"; }
+    { hostName = "plato.duckdns.org"; maxJobs = 8; sshKey = "/root/.ssh/id_rsa"; sshUser = "leroy"; system = "x86_64-linux"; }
     { hostName = "peaches"; maxJobs = 2; sshKey = "/root/.ssh/id_rsa"; sshUser = "leroy"; system = "x86_64-linux"; }
   ];
 }
