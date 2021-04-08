a21a47f9b11fc0870b595f6d2826963e591eb20a
Leroy Hopson <git@leroy.geek.nz>
2021-04-08 11:23:51 +0700

    Update workstation-extra role

--- roles/workstation-extra.nix
+++ roles/workstation-extra.nix
@@ -2,25 +2,78 @@
 
 {
   environment.systemPackages = with pkgs; [
+    audacity
+    audio-recorder
+    appimage-run
+    blender
+    calibre
+    chromium
+    discord
+    espeak
+    fahcontrol
+    fahviewer
+    ffmpeg
     gcc
     gimp
+    google-chrome
+    hfsprogs
     inkscape
+    krita
     libreoffice
-    nixopsUnstable
-    nodejs
-    npm2nix
-    (pkgs.texLiveAggregationFun { paths = [ pkgs.texLive pkgs.texLiveExtra pkgs.texLiveBeamer ]; })
+    lm_sensors
+    nvtop
+    pandoc
+    (qemu_kvm.override { pulseSupport = true; spiceSupport = true; })
+    python3
+    python3Packages.virtualenv
+    python3Packages.virtualenvwrapper
+    scrcpy
+    skype
+    spice
+    spotify
     taskwarrior
+    thunderbird
     transmission
+    virtmanager
+    virtviewer
     weechat
     xscreensaver
+
+    # Latex.
+    (texlive.combine {
+      inherit (texlive)
+        scheme-medium
+
+        # Dependencies of usecases style.
+        booktabs
+        multirow
+
+        # PlantUML plugin and its dependencies.
+        plantuml
+        adjustbox
+        collectbox
+        currfile
+        fvextra
+        pgfopts
+        upquote
+        xstring
+      ;
+    })
   ];
 
   virtualisation.docker.enable = true;
-  virtualisation.virtualbox.host.enable = true;
-  virtualisation.libvirtd.enable = true;
+  virtualisation.docker.enableNvidia = true;
+  virtualisation.docker.storageDriver = "devicemapper";
+  virtualisation.docker.listenOptions = [
+    "/run/docker.sock" "127.0.0.1:4153"
+  ];
+  virtualisation.virtualbox = {
+    host.enable = true;
+  };
 
-  # In order to pass USB devices from the host to the guests,
-  # the user needs to be in the vboxusers group
-  users.extraGroups.vboxusers.members = [ "leroy" ];
+  # Enable dconf so that virt-managers settings are saved.
+  programs.dconf.enable = true;
+
+  # Add user to all applicable groups.
+  users.users.leroy.extraGroups = [ "docker" "libvirtd" "usb" "kvm" "vboxusers" ];
 }
