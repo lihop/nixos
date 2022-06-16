3f9ca43ad9d1135446f277c93d931f5d9dde7582
Leroy Hopson <git@leroy.geek.nz>
2022-06-16 17:24:01 +0700

    Print correct relative path to generated ISO image
    Update script to print the relative path to the generated ISO image from
    the users current working directory rather than the directory that
    contains the script itself.

--- iso/geniso.sh
+++ iso/geniso.sh
@@ -4,7 +4,8 @@
 # Script to generate bootable NixOS installer ISO image.
 
 set -e
+start_dir=$(pwd)
 cd "$(dirname "$0")"
 echo -e "Generating NixOS ISO...\n"
 nixos-generate --format iso --configuration ./iso.nix -o result
-echo -e "\nISO generated: ./result/iso/$(ls -1 result/iso)"
+echo -e "\nISO generated: ./$(realpath -s --relative-to=${start_dir} ./result/iso/$(ls -1 result/iso))"
