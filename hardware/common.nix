0d2145b79c5142888d6350e2332e4d142bf4bd97
Leroy Hopson <git@leroy.geek.nz>
2022-06-21 14:57:57 +0700

    Add 'delayacct' kernel param
    Enables kernel config option CONFIG_TASK_DELAY_ACCT required by iotop.

--- hardware/common.nix
+++ hardware/common.nix
@@ -3,5 +3,8 @@
 {
   boot.kernelPackages = pkgs.linuxPackages_5_17;
   boot.loader.timeout = lib.mkForce 1;
+  boot.kernelParams = [
+    "delayacct" # Required by iotop.
+  ];
   imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 }
