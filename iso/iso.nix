e5403f7505e5c885bfcff68ec7ffa3fdf9ac5def
Leroy Hopson <git@leroy.geek.nz>
2023-02-25 17:26:06 +1300

    Enable cloud-init on ISO

--- iso/iso.nix
+++ iso/iso.nix
@@ -6,4 +6,10 @@
     ../hardware/common.nix
     ../roles/common.nix
   ];
+
+  # Enable cloud-init so ISO can be used with cloud providers (e.g. Vultr).
+  services.cloud-init = {
+    enable = true;
+    btrfs.enable = true;
+  };
 }
